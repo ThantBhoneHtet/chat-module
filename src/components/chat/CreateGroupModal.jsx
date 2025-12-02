93ebffbf66ed381e30fa1201d4213931cdf3475c
Thant Bhone Htet <thant2004bhone@gmail.com>
2025-12-03 00:14:15 +0630

    fix

--- src/components/chat/CreateGroupModal.jsx
+++ src/components/chat/CreateGroupModal.jsx
@@ -105,6 +105,7 @@ export function CreateGroupModal({
       
       const chatRequest = {
         type: 'GROUP',
+        owner: currentUser.userId,
         name: groupName.trim(),
         participantIds: participantIds
       };
